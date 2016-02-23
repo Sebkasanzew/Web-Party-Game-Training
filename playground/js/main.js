c094ba3bc9f8d3fd3f799458626b0663a649508a
Sebastian Kasanzew <sebkasanzew@gmail.com>
2016-02-23 23:18:13 +0100

    Assignment 1 ready to solve

---
+++ playground/js/main.js
@@ -0,0 +1,14 @@
+/* Task 6:
+ * Change the HTML-content of the clicked element to
+ * "<a target='_blank' href='http://www.google.de'>go to Google</a>"
+ */
+
+/* uncomment this, if you downloaded jQuery and added the scrip to index.html
+$().ready(function() { // execute the code in this block only when the DOM has finished loading
+    $(".i-am-a-div-with-a-class").on("click", function(event) {
+        console.log("the ID of the clicked element:", event.target.id);
+        // Tip: You can get the current jQuery-element with $(this)
+
+    })
+});
+*/
